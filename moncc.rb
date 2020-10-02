82211c3b3fccf1126ff899361ffc2a6cc1122764
Marcin Gasperowicz <xnooga@gmail.com>
2020-10-02 13:49:11 +0200

    Update description

--- moncc.rb
+++ moncc.rb
@@ -1,5 +1,5 @@
 class Moncc < Formula
-  desc "Moncc the container orchestrator 2.0"
+  desc "Moncc composes, deploys and manages your entire stack, on your own infrastructure, with nearly zero DevOps overhead."
   homepage "https://moncc.io"
   url "https://releases.moncc.io/stable/macos/moncc-darwin-v2.1.0.tar.gz"
   sha256 "e9f2565fb9fd7326858fa2f64b3639534ca0018121da770deeb7179961ac4ee4"
