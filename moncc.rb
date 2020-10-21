4f45395b8e2d1389b6f2781b98424ba98ad209d1
Luke Goral <luke.oidhche@gmail.com>
2020-10-21 17:46:55 +0200

    fix sha

--- moncc.rb
+++ moncc.rb
@@ -2,7 +2,7 @@ class Moncc < Formula
   desc "Moncc composes, deploys and manages your entire stack, on your own infrastructure, with nearly zero DevOps overhead."
   homepage "https://moncc.io"
   url "https://releases.moncc.io/stable/macos/moncc-darwin-v2.1.17.tar.gz"
-  sha256 "515d7672af480f8231fdd8cbebe9792dd9f049ce7652d2afd50efe54b2e33bf3"
+  sha256 "9174ad94505a976d619eebca9473562b90177051980121930cda0e155de562a2"
   version "2.1.17"
 
   bottle :unneeded
