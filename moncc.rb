794d5fc677d6fe663f012ec1c53a9dd821fc685c
Luke Goral <luke.oidhche@gmail.com>
2020-11-05 15:42:01 +0100

    updated for v2.2.0

--- moncc.rb
+++ moncc.rb
@@ -1,9 +1,9 @@
 class Moncc < Formula
   desc "Moncc composes, deploys and manages your entire stack, on your own infrastructure, with nearly zero DevOps overhead."
   homepage "https://moncc.io"
-  url "https://releases.moncc.io/stable/macos/moncc-darwin-v2.1.25.tar.gz"
-  sha256 "bf8918883020fc9c329546c9ac56b1b34d15dce7b6677f998bb5e233b445d911"
-  version "2.1.25"
+  url "https://releases.moncc.io/stable/macos/moncc-darwin-v2.2.0.tar.gz"
+  sha256 "60d842f88249be4bc4a009a93bc7ac2a7efcdcc200328d3c49ef85dfcf64288c"
+  version "2.2.0"
 
   bottle :unneeded
 
