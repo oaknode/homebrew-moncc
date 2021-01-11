7e6f29d8ac687775411d6f3b90b97e90f481abd5
Circle CI job <cicd@oaknode.com>
2021-01-11 10:59:30 +0000

    updated for v2.3.0

--- moncc.rb
+++ moncc.rb
@@ -1,9 +1,9 @@
 class Moncc < Formula
   desc "Moncc composes, deploys and manages your entire stack, on your own infrastructure, with nearly zero DevOps overhead."
   homepage "https://moncc.io"
-  url "https://releases.moncc.io/stable/macos/moncc-darwin-v2.2.22.tar.gz"
-  sha256 "c7f24cc1f55b043134be551ff1604c2ed2def8ea31dc421d0d65a8849fb32348"
-  version "2.2.22"
+  url "https://releases.moncc.io/stable/macos/moncc-darwin-v2.3.0.tar.gz"
+  sha256 "6f3b509a6fe594d8128f1049cb3a34ec92b3c5a4ca621a26b4eaaf469a7476e8"
+  version "2.3.0"
 
   bottle :unneeded
 
